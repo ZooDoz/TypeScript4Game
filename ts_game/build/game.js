4546e109d6ae679c035af91ed7b8ecfc30451d63
ZooDoz <513828621@qq.com>
2018-01-23 19:34:21 +0800

    简单修正

--- ts_game/build/game.js
+++ ts_game/build/game.js
@@ -1,11 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var _ = require("lodash");
 var CardBox = /** @class */ (function () {
     function CardBox(size) {
         this.cards = [size];
         for (var i = 0; i < size; i++)
             this.cards[i] = i;
+        _.shuffle(this.cards);
     }
+    CardBox.prototype.show = function () {
+        for (var i in this.cards)
+            console.log(i);
+    };
     return CardBox;
 }());
 exports.CardBox = CardBox;
+var cb = new CardBox(54);
+cb.show();
--- ts_game/src/game.ts
+++ ts_game/src/game.ts
@@ -1,4 +1,4 @@
-import * as lodash from 'lodash'
+import * as _ from 'lodash'
 
 class CardBox
 {
@@ -8,8 +8,15 @@ class CardBox
         this.cards=[size];
         for(var i = 0 ; i < size ; i ++)
            this.cards[i]=i;
-        
+        _.shuffle(this.cards);
+    }
+
+    show(): void{
+        for(var i in this.cards)
+            console.log(i);
     }
 }
 
+var cb = new CardBox(54);
+cb.show();
 export{CardBox}
